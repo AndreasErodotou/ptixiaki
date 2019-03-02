d54196e3ee46f7db15e47bd300b3d8e74ada5ff9
Andreas <andreasero71@gmail.com>
2019-03-02 03:25:58 +0200

    java connected to the database

--- src/main/java/hy499/test_ptixiaki/test/sparkMain.java
+++ src/main/java/hy499/test_ptixiaki/test/sparkMain.java
@@ -1,5 +1,10 @@
 package hy499.test_ptixiaki.test;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +16,31 @@ package hy499.test_ptixiaki.test;
  * @author Andreas
  */
 public class sparkMain {
-    public static void main(String[] args) {
+
+    Connection connection;
+    String url = "jdbc:postgresql://localhost:5432/ptixiaki";
+    String username = "postgres";
+    String password = "AndHer123!";
+
+    public Connection databaseConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection(url, username, password);
+        return connection;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        sparkMain spMain = new sparkMain();
+        if (spMain.databaseConnection() == null) {
+            System.out.println("connection failed");
+        } else {
+            System.out.println("connection succeded");
+        }
+        PreparedStatement stmt;
+        stmt = spMain.connection.prepareStatement("SELECT * FROM public.\"BID\"");
+        ResultSet res = stmt.executeQuery();
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getDouble(2) + "\t" + res.getDate(3) + "\t" + res.getBoolean(4) + "\t" + res.getString(5) + "\t" + res.getString(6));
+        }
 //        path("/api", () -> {
 //            before("/*", (q, a) -> log.info("Received api call"));
 //            path("/email", () -> {
